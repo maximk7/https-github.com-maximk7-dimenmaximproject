260ac8a5e321d97e7072a2990ea01f6e1b79c321
Maxim Krymchansky <maxim.krymchansky@student.rocva.nl>
2025-09-30 11:41:25 +0200

    ja

--- includes/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_file = __DIR__ . '/../db/maaltijdplanner.db';
-try {
-    $pdo = new PDO("sqlite:" . $db_file);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die("Kan geen verbinding maken met database: " . $e->getMessage());
-}
-?>
